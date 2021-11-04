38d145f78ef1d53703c6dcb56b722a5a348387a9
ray <liucjray@gmail.com>
2021-11-04 21:15:37 +0800

    新增 SendText 供外部服務請求後發送訊息

--- gogogo/index.go
+++ gogogo/index.go
@@ -1,6 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var (
+	token string
+)
 
 func main() {
 	r := gin.Default()
@@ -9,9 +24,9 @@ func main() {
 
 	r.GET("/", index)
 	r.GET("/ping", ping)
-	r.GET("/ping2", ping2)
+	r.Any("/api/line/send_text", SendText)
 
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":" + os.Getenv("PORT"))
 }
 
 func index(c *gin.Context) {
@@ -22,12 +37,59 @@ func index(c *gin.Context) {
 
 func ping(c *gin.Context) {
 	c.JSON(200, gin.H{
-		"message": "pong1",
+		"message": "pong",
 	})
 }
 
-func ping2(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong2",
-	})
+func SendText(c *gin.Context) {
+
+	// token = os.Getenv("LINE_NOTIFY_ACCESS_TOKEN")
+
+	token := c.DefaultPostForm("token", "")
+	text := c.DefaultPostForm("text", "")
+
+	if token == "" || text == "" {
+		fmt.Println("幹智障喔")
+	}
+
+	v := url.Values{}
+	v.Set("message", text)
+	Request("POST", "https://notify-api.line.me/api/notify", v, token)
+
+}
+
+func Request(method string, url string, data url.Values, token string) ([]byte, error) {
+	client := &http.Client{}
+
+	res, err := http.NewRequest(method, url, strings.NewReader(data.Encode()))
+
+	if err != nil {
+		return nil, err
+	}
+
+	res.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	if token != "" {
+		res.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	}
+
+	resp, err := client.Do(res)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Println(err.Error())
+		}
+	}()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
 }
